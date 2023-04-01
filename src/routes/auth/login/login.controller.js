5f2ee1cd84dd18fced7c891a6674a0ffc6bc6379
ruona <ogheneruonaagadagba4@gmail.com>
2023-04-01 10:57:06 +0100

    added the functionality for login for both agent and customers

--- src/routes/auth/login/login.controller.js
+++ src/routes/auth/login/login.controller.js
@@ -12,7 +12,7 @@ async function agentLogin(req, res) {
 
   try {
     //find the agent in the database
-    const agent = await db("users").where({ email }).first();
+    const agent = await db("agent").where({ email }).first();
 
     if (!agent) {
       return res.status(401).json("support agent not found");
@@ -43,6 +43,47 @@ async function agentLogin(req, res) {
   }
 }
 
+async function customerLogins(req, res) {
+  const { email, passwords } = req.body;
+
+  if (!email || !passwords) {
+    return res.status(400).json("email and passwords are required");
+  }
+
+  try {
+    //find the agent in the database
+    const customer = await db("customers").where({ email }).first();
+
+    if (!customer) {
+      return res.status(401).json("support agent not found");
+    }
+
+    //check if the password is correct
+    const passwordMatch = await bcrypt.compare(passwords, customer.passwords);
+    if (!passwordMatch) {
+      return res.status(401).json("invalid email or password");
+    }
+
+    //generate a jwt token
+    const token = jwt.sign(
+      { customerId: customer.customer_id, roles: customer.roles },
+      "your_secret_key"
+    );
+
+    //this is to decode the token to check if the id and roles are stored there
+    //const decode = jwt.decode(token)
+    //console.log(decode.agentId)
+
+    //return the agent details with token
+    const customerWithToken = { ...customer, token };
+    return res.status(200).json(customerWithToken);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "An error occurred" });
+  }
+}
+
 module.exports = {
   agentLogin,
+  customerLogins,
 };
--- src/routes/auth/login/login.router.js
+++ src/routes/auth/login/login.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const agentLoginRouter = express.Router();
 
-const { agentLogin } = require("./login.controller");
+const { agentLogin, customerLogins } = require("./login.controller");
 
 agentLoginRouter.post("/agent/login", agentLogin);
+agentLoginRouter.post("/customer/login", customerLogins);
 
 module.exports = {
   agentLoginRouter,
