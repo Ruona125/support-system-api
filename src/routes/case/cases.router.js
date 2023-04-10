e2054682efb52c82257328a69c65ad3650912aeb
ruona <ogheneruonaagadagba4@gmail.com>
2023-04-10 22:24:57 +0100

    added the feature for admin to view casesg

--- src/routes/case/cases.router.js
+++ src/routes/case/cases.router.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const caseRouter = express.Router();
 
-const { agentAuth, verifyCertainToken } = require("../../utils/requireAuth");
+const {
+  agentAuth,
+  verifyCertainToken,
+  adminAuth,
+} = require("../../utils/requireAuth");
 const {
   createCase,
   closeCase,
@@ -12,6 +16,7 @@ const {
 caseRouter.post("/case", createCase);
 caseRouter.post("/case/complete/:case_id", closeCase);
 caseRouter.get("/case", agentAuth, viewCases);
+caseRouter.get("admin/case", adminAuth, viewCases);
 caseRouter.get("/case/:case_id", certainCase);
 
 module.exports = {
